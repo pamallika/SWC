1bb600e114927ea2143cb7940aea586125070af4
Alexandr Fedorenko <sasha.fedorenko4736@gmail.com>
2023-09-26 13:47:03 +0300

    add adminLte

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
